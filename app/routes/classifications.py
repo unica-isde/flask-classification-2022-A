dca44a2a804d497573b2dc514896590930362552
hemnemne <32846305+hemnemne@users.noreply.github.com>
2022-12-07 12:17:37 +0100

    Revert "Merging branch #4 into the master."

--- app/routes/classifications.py
+++ app/routes/classifications.py
@@ -1,5 +1,3 @@
-import os
-
 import redis
 from flask import render_template
 from rq import Connection, Queue
@@ -12,13 +10,12 @@ from config import Configuration
 
 config = Configuration()
 
+
 @app.route('/classifications', methods=['GET', 'POST'])
 def classifications():
-    """
-    API for selecting a model and an image from the
-    'imagenet_subset' folder and running a classification job.
-    Returns the output scores from the model.
-    """
+    """API for selecting a model and an image and running a 
+    classification job. Returns the output scores from the 
+    model."""
     form = ClassificationForm()
     if form.validate_on_submit():
         image_id = form.image.data
